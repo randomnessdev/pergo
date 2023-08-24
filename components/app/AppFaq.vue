c9337cacb49df65db848bd1b3437889316b1ee77
Guillaume <randomnessdev@gmail.com>
2023-08-24 18:43:04 +0200

    fix

--- components/app/AppFaq.vue
+++ components/app/AppFaq.vue
@@ -13,13 +13,6 @@ defineProps({
 
 <template>
   <section class="container relative flex flex-col pt-24 pb-4 transition duration-700 translate-y-0 opacity-100 sm:pb-8 md:pb-12 lg:pb-20">
-    <ContentSlot :use="$slots.root" />
-    <h2 class="max-w-lg pb-6 text-3xl font-semibold md:text-4xl lg:text-5xl sm:max-w-xl md:max-w-3xl lg:max-w-4xl u-text-gray-900">
-      <ContentSlot :use="$slots.title" unwrap="p" />
-    </h2>
-    <p class="max-w-3xl mb-12 text-xl">
-      <ContentSlot :use="$slots.description" unwrap="p" />
-    </p>
     <div class="gap-16 columns-2">
       <slot />
     </div>
